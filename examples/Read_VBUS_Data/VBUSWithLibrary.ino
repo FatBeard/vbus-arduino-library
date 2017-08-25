aa2fe4613abe060a77eb74d5c5e96c995baa6c80
Brian Healy <692143+FatBeard@users.noreply.github.com>
2017-08-25 23:49:31 +0100

    Add example of how to use library.

---
+++ examples/Read_VBUS_Data/VBUSWithLibrary.ino
@@ -0,0 +1,44 @@
+#define DEBUG 1
+#include "VBUSDecoder.h"
+
+VBUSDecoder vb;
+
+void setup()
+{
+
+  vb;
+
+#if DEBUG
+  Serial.begin(9600);
+#endif
+  vb.initialise();
+
+}
+
+void loop()
+{
+  vb.readSensor();
+  Serial.println("");
+  Serial.println("*** Fresh Read ***");
+  Serial.print("Collector Temp: ");
+  Serial.println(vb.getS1Temp());
+  Serial.print("Top Tank Temp: ");
+  Serial.println(vb.getS3Temp());
+  Serial.print("Bottom Tank Temp: ");
+  Serial.println(vb.getS2Temp());
+  Serial.print("Water Pump Status: ");
+  Serial.println(vb.getP1Status());
+  Serial.print("Water Pump Speed: ");
+  Serial.println(vb.getP1Speed());
+  Serial.print("Pump Hours Operation: ");
+  Serial.println(vb.getP1OperatingHours());
+  Serial.print("System Alert Status: ");
+  Serial.println(vb.getAlertStatus());
+  Serial.print("System Time: ");
+  Serial.println(vb.getSystemTime());
+  Serial.println("*** End Read ***");
+
+
+  delay(20000);
+}
+
